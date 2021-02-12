810b1306ecf1c51b3a3cbb6d6c66b1b5ffcad497
Wilder <friendwilder@yahoo.com.mx>
2021-02-11 19:16:41 -0600

    Arithmetic expression exercise completed and Comparing strings ignoring whitespaces starter code

--- Problems/Arithmetic expression/src/Main.java
+++ Problems/Arithmetic expression/src/Main.java
@@ -4,5 +4,7 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
+        int myValue = scanner.nextInt();
+        System.out.printf("%d", ((myValue + 1) * myValue + 2) * myValue + 3);
     }
 }
