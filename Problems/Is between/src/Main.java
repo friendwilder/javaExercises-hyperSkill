609e3520b37157166a514b0a21afe60772bdbbc9
Wilder <friendwilder@yahoo.com.mx>
2021-02-11 19:00:33 -0600

    Is between exercise completed

--- Problems/Is between/src/Main.java
+++ Problems/Is between/src/Main.java
@@ -4,5 +4,12 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+
+        System.out.println((number1 >= number2 && number1 <= number3 ||
+            number1 >= number3 && number1 <= number2)? true : false);
+
     }
 }
